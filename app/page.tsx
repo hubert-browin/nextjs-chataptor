9f20ede471fca19c4d8a8d77b5671a36e12cc727
hubert-browin <hubert.dziemiach@browin.pl>
2026-01-21 13:59:47 +0100

    final upt

--- app/page.tsx
+++ app/page.tsx
@@ -585,8 +585,8 @@ export default function Home() {
                                     Wo ist mein Paket?
                                     <div className="h-px bg-slate-100 my-2"></div>
                                     <div className="text-indigo-600 font-medium flex items-center gap-2">
-                                        Gdzie jest moja paczka?
                                         <svg className="w-3.5 h-3.5 text-indigo-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129"></path></svg>
+                                        Gdzie jest moja paczka?
                                     </div>
                                 </div>
                             </div>
@@ -600,8 +600,8 @@ export default function Home() {
                                     Sprawdzam to...
                                     <div className="h-px bg-white/20 my-2"></div>
                                     <div className="text-indigo-200 font-medium flex items-center gap-2">
-                                        Ich überprüfe das...
                                         <svg className="w-3.5 h-3.5 text-indigo-300" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129"></path></svg>
+                                        Ich überprüfe das...
                                     </div>
                                 </div>
                             </div>
